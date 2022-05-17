ced6d2c003292e5b73af00d869aa35749d36a52c
aemem <anamoliner.marques@gmail.com>
2022-05-17 18:54:35 +0000

    dropdowns funcionan

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,8 +3,52 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <p>Navbar</p>
+    <nav className="navbar navbar-light bg-light fixed-top" id="navbar">
+      <div>
+        <button
+          className="btn btn-light"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i className="fa-solid fa-bars"></i>
+        </button>
+        <ul
+          className="dropdown-menu"
+          id="menu-dropdown"
+          aria-labelledby="dropDownMenu"
+        >
+          <Link to="/">
+            <li>Inicio</li>
+          </Link>
+          <li>Bio</li>
+          <li>Servicios</li>
+          <li>Documentos</li>
+          <li>FAQ</li>
+        </ul>
+      </div>
+
+      <div>
+        <button
+          className="btn btn-secondary rounded-circle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          id="profile-menu"
+        >
+          <i className="fa-solid fa-user"></i>
+        </button>
+        <ul
+          className="dropdown-menu"
+          id="profile-dropdown"
+          aria-labelledby="dropDownMenu"
+        >
+          <li>Iniciar Sesión</li>
+          <li>Registrarse</li>
+          <li>Cerrar Sesión</li>
+          <li>Mi Perfil</li>
+        </ul>
+      </div>
     </nav>
   );
 };
