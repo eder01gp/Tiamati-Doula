ed42fe4ad249c85d4d2656b02b84012d5287805d
aemem <anamoliner.marques@gmail.com>
2022-05-31 18:28:20 +0000

    whatsapp added

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import whatsapp from "../../../img/logo/whatsapp.png";
+import insta from "/workspace/Tiamati-Doula/src/img/logo/Instagram.png";
 
 export const Footer = () => (
   <footer className="footer text-center fixed-bottom">
-    <h2>Contacto</h2>
+    <h2 id="contacto-title">Contacto</h2>
     <h4 id="footer-phone">+34 644 75 46 71</h4>
     <h4 id="footer-email">hello@tiamati.com</h4>
     <Link to="" id="footer-insta-link">
-      <i class="fa-brands fa-instagram"></i>
+      <img src={insta} id="insta-logo" />
     </Link>
+
+    <a
+      href="https://wa.me/34636519226"
+      className="whatsapp_float fixed-bottom"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={whatsapp} alt="whatsapp logo" id="whatsapp-logo" />
+    </a>
   </footer>
 );
