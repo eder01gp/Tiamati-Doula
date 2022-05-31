4d9f2f1b50e45a198beab91d76734a170af02d15
aemem <anamoliner.marques@gmail.com>
2022-05-31 16:59:35 +0000

    primero

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
-  <footer className="footer text-center">
-    <p>Footer</p>
+  <footer className="footer text-center fixed-bottom">
+    <h2>Contacto</h2>
+    <h4 id="footer-phone">+34 644 75 46 71</h4>
+    <h4 id="footer-email">hello@tiamati.com</h4>
+    <Link to="" id="footer-insta-link">
+      <i class="fa-brands fa-instagram"></i>
+    </Link>
   </footer>
 );
