f466516dae46eead2c067da9745b8c75dea2300c
aemem <anamoliner.marques@gmail.com>
2022-05-31 17:04:23 +0000

    map funciona

--- src/front/js/pages/faq.js
+++ src/front/js/pages/faq.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/faq.css";
 import { Navbar } from "../component/navbar";
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 export const Faq = () => {
   const { store, actions } = useContext(Context);
+  const { show, setShow } = useState(false);
 
   return (
     <div className="container">
@@ -15,133 +16,81 @@ export const Faq = () => {
       {/* FAQ usuarias */}
       <div id="faq-users">
         <h3>Usuarias</h3>
-        <div className="accordion" id="user-questions">
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse show"
-              aria-labelledby="headingOne"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                Sed placerat ante in orci varius fringilla. Nullam varius ligula
-                nec rutrum convallis. Nam est neque, semper vitae velit nec,
-                accumsan scelerisque mi. Integer egestas vestibulum posuere.
-                Curabitur laoreet, lacus ut iaculis consectetur, odio dui
-                posuere lacus, a molestie lorem ex at justo. Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit.
+        {store.faq.map((faqs) => {
+          return (
+            <div className="accordion" id="user-questions" key={faqs.id}>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id={faqs.question_id}>
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`${"#"}` + faqs.answer_id}
+                    aria-expanded="false"
+                    aria-controls={faqs.answer_id}
+                    onClick={() => {
+                      setShow === true;
+                    }}
+                  >
+                    {faqs.question}
+                  </button>
+                </h2>
+                <div
+                  id={faqs.answer_id}
+                  className={
+                    show === true
+                      ? "accordion-collapse collapse show"
+                      : "accordion-collapse collapse"
+                  }
+                  aria-labelledby={faqs.question_id}
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">{faqs.answer}</div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingTwo">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="true"
-                aria-controls="collapseTwo"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-              </button>
-            </h2>
-            <div
-              id="collapseTwo"
-              className="accordion-collapse collapse show"
-              aria-labelledby="headingTwo"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                Sed placerat ante in orci varius fringilla. Nullam varius ligula
-                nec rutrum convallis. Nam est neque, semper vitae velit nec,
-                accumsan scelerisque mi. Integer egestas vestibulum posuere.
-                Curabitur laoreet, lacus ut iaculis consectetur, odio dui
-                posuere lacus, a molestie lorem ex at justo. Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit.
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
 
       {/* FAQ empresas */}
       <div id="faq-business">
         <h3>Empresas</h3>
-        <div className="accordion" id="business-questions">
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingThree">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseThree"
-                aria-expanded="true"
-                aria-controls="collapseThree"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-              </button>
-            </h2>
-            <div
-              id="collapseThree"
-              className="accordion-collapse collapse show"
-              aria-labelledby="headingThree"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                Sed placerat ante in orci varius fringilla. Nullam varius ligula
-                nec rutrum convallis. Nam est neque, semper vitae velit nec,
-                accumsan scelerisque mi. Integer egestas vestibulum posuere.
-                Curabitur laoreet, lacus ut iaculis consectetur, odio dui
-                posuere lacus, a molestie lorem ex at justo. Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit.
+        {store.bfaq.map((bfaqs) => {
+          return (
+            <div className="accordion" id="business-questions" key={bfaqs.id}>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id={bfaqs.question_id}>
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`${"#"}` + bfaqs.answer_id}
+                    aria-expanded="false"
+                    aria-controls={bfaqs.answer_id}
+                    onClick={() => {
+                      setShow === true;
+                    }}
+                  >
+                    {bfaqs.question}
+                  </button>
+                </h2>
+                <div
+                  id={bfaqs.answer_id}
+                  className={
+                    show === true
+                      ? "accordion-collapse collapse show"
+                      : "accordion-collapse collapse"
+                  }
+                  aria-labelledby={bfaqs.question_id}
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">{bfaqs.answer}</div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingFour">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseFour"
-                aria-expanded="true"
-                aria-controls="collapseFour"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-              </button>
-            </h2>
-            <div
-              id="collapseFour"
-              className="accordion-collapse collapse show"
-              aria-labelledby="headingFour"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                Sed placerat ante in orci varius fringilla. Nullam varius ligula
-                nec rutrum convallis. Nam est neque, semper vitae velit nec,
-                accumsan scelerisque mi. Integer egestas vestibulum posuere.
-                Curabitur laoreet, lacus ut iaculis consectetur, odio dui
-                posuere lacus, a molestie lorem ex at justo. Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit.
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
