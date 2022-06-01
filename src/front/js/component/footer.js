9d2664c8fb334e1f350695708a20c55712494b3c
aemem <anamoliner.marques@gmail.com>
2022-06-01 17:05:41 +0000

    insta link added

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,16 +4,21 @@ import whatsapp from "../../../img/logo/whatsapp.png";
 import insta from "/workspace/Tiamati-Doula/src/img/logo/Instagram.png";
 
 export const Footer = () => (
-  <footer className="footer text-center fixed-bottom">
+  <footer className="footer text-center position-absolute bottom-0 start-0">
     <h2 id="contacto-title">Contacto</h2>
     <h4 id="footer-phone">+34 644 75 46 71</h4>
     <h4 id="footer-email">hello@tiamati.com</h4>
-    <Link to="" id="footer-insta-link">
+    <Link
+      id="footer-insta-link"
+      onClick={() => {
+        window.open("https://www.instagram.com/doulatiamati/");
+      }}
+    >
       <img src={insta} id="insta-logo" />
     </Link>
 
     <a
-      href="https://wa.me/34636519226"
+      href="https://wa.me/34615077934"
       className="whatsapp_float fixed-bottom"
       target="_blank"
       rel="noopener noreferrer"
