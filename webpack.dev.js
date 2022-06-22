83273f0d38b2944638af8fa85612f6134797f8e7
ederdon <ederpalacin@gmail.com>
2022-06-22 09:10:35 +0000

    webpack dev commented

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,10 +6,10 @@ const common = require('./webpack.common.js');
 
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
-if(process.env.GITPOD_WORKSPACE_URL){
+/* if(process.env.GITPOD_WORKSPACE_URL){
   const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
   publicUrl = `wss://${port}-${host}/ws`;
-}
+} */
 
 module.exports = merge(common, {
     mode: 'development',
