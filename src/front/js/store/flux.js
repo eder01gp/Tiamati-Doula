92a4680160da5e1a333ca37a53e947d111c840da
ederdon <ederpalacin@gmail.com>
2022-06-28 10:18:35 +0000

    develop3 working

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,7 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       url:
-        process.env.DOMAIN +
+      "https://3001-4geeksacade-reactflaskh-g28jy9vbgjl.ws-eu47.gitpod.io/" +
         "api",
       logged: null,
       token: null,
